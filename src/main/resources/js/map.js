ec7653232069fe4c766a7f9b9476587e46b0df9c
Patman1O1 <patrickthomas101x@gmail.com>
2026-02-20 20:27:05 -0600

    Edit map.js

--- src/main/resources/js/map.js
+++ src/main/resources/js/map.js
@@ -3,34 +3,35 @@ let markers = [];
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 40.7128, lng: -74.0060 }, // New York
+        center: { lat: 40.7128, lng: -74.0060 },
         zoom: 12
     });
 
-    // Click listener - sends data back to Java
     map.addListener("click", function(event) {
         const lat = event.latLng.lat();
         const lng = event.latLng.lng();
 
         addMarker(lat, lng);
-        // Call Java method via JSBridge
+
         if (window.javaApp) {
             window.javaApp.onMapClick(lat, lng);
         }
     });
 }
 
-// Called from Java to add a marker
 function addMarker(lat, lng, title) {
     const marker = new google.maps.Marker({
-    position: { lat: lat, lng: lng }, map: map, title: title || "" });
+        position: { lat: lat, lng: lng },
+        map: map,
+        title: title || ""
+    });
     markers.push(marker);
 }
 
-// Called from Java to move/center the map
-function panTo(lat, lng) { map.panTo({ lat: lat, lng: lng }); }
+function panTo(lat, lng) {
+    map.panTo({ lat: lat, lng: lng });
+}
 
-// Called from Java to clear all markers
 function clearMarkers() {
     markers.forEach(m => m.setMap(null));
     markers = [];
