9b55684af3e63e9b085ac22fac89d65ceb335226
Patman1O1 <patrickthomas101x@gmail.com>
2026-03-14 17:35:54 -0500

    Add stage manager

--- src/main/java/edu/uic/cs342/project2/frontend/WeatherApplication.java
+++ src/main/java/edu/uic/cs342/project2/frontend/WeatherApplication.java
@@ -1,4 +1,4 @@
-package edu.cs342.project2.frontend;
+package edu.uic.cs342.project2.frontend;
 
 import javafx.application.Application;
 import javafx.scene.text.Font;
@@ -6,19 +6,18 @@ import javafx.stage.Stage;
 
 public class WeatherApplication extends Application {
     // ── Fields ──────────────────────────────────────────────────────────────────────────────────────────────────────
-    private SceneManager sceneManager;
+    private final StageManager stageManager;
+
+    // ── Constructors ─────────────────────────────────────────────────────────────────────────────────────────────────
+    private WeatherApplication() { this.stageManager = StageManager.getInstance(); }
 
     // ── Methods ──────────────────────────────────────────────────────────────────────────────────────────────────────
-    public static void main(String[] args) { launch(args); }
+    public static void main(String[] args) { Application.launch(args); }
 
     @Override
     public void start(Stage primaryStage) {
         try {
-            // Load fonts
-            Font.loadFont(getClass().getResourceAsStream("/fonts/open-sans/OpenSans-Regular.ttf"), 14);
-
-            this.sceneManager = SceneManager.getInstance();
-            this.sceneManager.setStage(primaryStage);
+            this.stageManager.load(primaryStage);
         } catch (Exception e) {
             e.printStackTrace();
         }
