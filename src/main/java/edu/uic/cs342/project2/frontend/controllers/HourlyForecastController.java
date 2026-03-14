6b5e74a5a244ccb97dcde5273e296658f9bf8f20
Patman1O1 <patrickthomas101x@gmail.com>
2026-03-14 17:39:56 -0500

    Implement Initializable

--- src/main/java/edu/uic/cs342/project2/frontend/controllers/HourlyForecastController.java
+++ src/main/java/edu/uic/cs342/project2/frontend/controllers/HourlyForecastController.java
@@ -1,4 +1,20 @@
 package edu.uic.cs342.project2.frontend.controllers;
 
-public class HourlyForecastController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HourlyForecastController implements Initializable {
+    // ── Methods ──────────────────────────────────────────────────────────────────────────────────────────────────────
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) throws NullPointerException {
+        if (url == null) {
+            throw new NullPointerException("url is null");
+        }
+
+        if (resourceBundle == null) {
+            throw new NullPointerException("resourceBundle is null");
+        }
+    }
 }
