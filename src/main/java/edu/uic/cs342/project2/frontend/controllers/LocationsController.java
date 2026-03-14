f94dbe05494bb541e42201e4f056e4d3da37ffd1
Patman1O1 <patrickthomas101x@gmail.com>
2026-03-14 17:40:30 -0500

    Implement Initializable

--- src/main/java/edu/uic/cs342/project2/frontend/controllers/LocationsController.java
+++ src/main/java/edu/uic/cs342/project2/frontend/controllers/LocationsController.java
@@ -1,4 +1,19 @@
 package edu.uic.cs342.project2.frontend.controllers;
 
-public class LocationsController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LocationsController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) throws NullPointerException {
+        if (url == null) {
+            throw new NullPointerException("url is null");
+        }
+
+        if (resourceBundle == null) {
+            throw new NullPointerException("resourceBundle is null");
+        }
+    }
 }
