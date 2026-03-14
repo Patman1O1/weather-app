70b9e4735b07dfb5ce1a74b16a0e9864b053d7b8
Patman1O1 <patrickthomas101x@gmail.com>
2026-03-14 17:42:36 -0500

    Implement Initializable

--- src/main/java/edu/uic/cs342/project2/frontend/controllers/SevenDayForecastController.java
+++ src/main/java/edu/uic/cs342/project2/frontend/controllers/SevenDayForecastController.java
@@ -1,4 +1,20 @@
 package edu.uic.cs342.project2.frontend.controllers;
 
-public class SevenDayForecastController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class SevenDayForecastController implements Initializable {
+    // ── Methods ──────────────────────────────────────────────────────────────────────────────────────────────────────
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) throws NullPointerException {
+        if (url == null) {
+            throw new NullPointerException("url is null");
+        }
+
+        if (resourceBundle == null) {
+            throw new NullPointerException("resourceBundle is null");
+        }
+    }
 }
