dc4e4dbe56d54d82a8bd07275b48502913fa66f1
Patman1O1 <patrickthomas101x@gmail.com>
2026-05-11 16:11:22 -0500

    Remove -rf

--- src/main/java/org/patman101/weather_app/models/measurements/UnitMismatchException.java
+++
@@ -1,6 +0,0 @@
-package org.patman101.weather_app.models.measurements;
-
-public class UnitMismatchException extends RuntimeException {
-    // ── Constructors ─────────────────────────────────────────────────────────────────────────────────────────────────
-    public UnitMismatchException(String message) { super(message); }
-}
