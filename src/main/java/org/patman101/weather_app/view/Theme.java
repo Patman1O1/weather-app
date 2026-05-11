e3027ae907b79ba1c757e431159b7f98a5c9fce9
Patman1O1 <patrickthomas101x@gmail.com>
2026-05-11 12:55:54 -0500

    Add Theme.java

---
+++ src/main/java/org/patman101/weather_app/view/Theme.java
@@ -0,0 +1,37 @@
+package org.patman101.weather_app.view;
+
+import javafx.application.Application;
+
+public class Theme {
+    // ── Fields ───────────────────────────────────────────────────────────────────────────────────────────────────────
+    private final String name, stylesheet, iconPath;
+
+    // ── Constructors ─────────────────────────────────────────────────────────────────────────────────────────────────
+    public Theme(String name, String stylesheet, String iconPath) {
+        this.name = name;
+        this.stylesheet = stylesheet;
+        this.iconPath = iconPath;
+    }
+
+    // ── Getters ──────────────────────────────────────────────────────────────────────────────────────────────────────
+    public String getIconPath() { return this.iconPath; }
+
+    // ── Methods ──────────────────────────────────────────────────────────────────────────────────────────────────────
+    @Override
+    public String toString() { return this.name; }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Theme other)) {
+            return false;
+        }
+
+        if (this == object) {
+            return true;
+        }
+
+        return this.name.equals(other.name);
+    }
+
+    public void apply() { Application.setUserAgentStylesheet(this.stylesheet); }
+}
